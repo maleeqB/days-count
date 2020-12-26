939f68171bcb06a28abe4d2989656be759c36cd0
mab <malikbello084@gmail.com>
2020-12-26 01:12:50 +0100

    format source file

--- Dob.py
+++ Dob.py
@@ -1,10 +1,17 @@
-import sys,datetime
-def mab(Y,M,D):
-	n=datetime.datetime.now()
-	b=datetime.datetime(Y,M,D)
-	g=n-b
-	print(str(g))
-Y=int(sys.argv[1])
-M=int(sys.argv[2])
-D=int(sys.argv[3])
-mab(Y,M,D)
+import datetime
+import sys
+
+
+def mab(y, m, d):
+    n = datetime.datetime.now()
+    b = datetime.datetime(y, m, d)
+    g = n - b
+    return str(g)
+
+
+Y = int(sys.argv[1])
+M = int(sys.argv[2])
+D = int(sys.argv[3])
+
+if __name__ == "__main__":
+    print(mab(Y, M, D))
