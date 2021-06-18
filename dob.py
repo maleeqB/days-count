6e5618bd0987ac60dbebd9c444ddbe339742f558
Mab <malikbello084@gmail.com>
2021-06-18 22:38:32 +0100

    rafactored code

--- dob.py
+++ dob.py
@@ -1,50 +1,62 @@
-import datetime
-
-class Dob:
-    def __init__(self):
-        self.welcome_text = """
-        DOB : print total days since day of Birth
-
-        day : Day of Birth
-        month : Month of Birth
-        year : Year Of Birth
-
-        e.g user given birth to on 2nd August 2001 has the following values
-        day: 2
-        month: 8
-        year: 2001
-
-        By: AbdulMalik Bello
-        """
-
-    def calc_dob(self):
-        day = self.get_input("Enter day ")
-        month = self.get_input("Enter month ")
-        year = self.get_input("Enter Year ")
-
-        today = datetime.datetime.now()
-        birthday = datetime.datetime(year, month, day)
-        
-        total_days = today - birthday
-        return total_days
+import textwrap
+from datetime import datetime
 
 
-    def get_input(self, msg):
-        user_input = ""
-        done = False 
-        while(not done):
-            inp = input(msg)
-            if inp.isdigit():
-                user_input = inp
-                done = True
-            else: print("Please Enter digits only")
-        return int(user_input)
+def get_welcome_text():
+    welcome_text = textwrap.dedent("""
+            print total days since day of Birth, and next birthday
 
-    def __repr__(self):
-        print(self.welcome_text)
-        total_days = self.calc_dob()
-        return str(total_days)
+            day : Day of Birth
+            month : Month of Birth
+            year : Year Of Birth
+
+            e.g user given birth to on 2nd August 2001 has the following values
+            day: 2
+            month: 8
+            year: 2001
+
+            By: AbdulMalik Bello
+            """)
+    return welcome_text
+
+
+def get_user_birthday():
+    day = get_int_input("Enter day ")
+    month = get_int_input("Enter month ")
+    year = get_int_input("Enter Year ")
+    return datetime(year, month, day)
+
+
+def calc_dates(birthday, now):
+
+    delta1 = datetime(now.year, birthday.month, birthday.day)
+    delta2 = datetime(now.year + 1, birthday.month, birthday.day)
+
+    total_days = now - birthday
+    next_birthday = (delta1 if delta1 > now else delta2) - now
+
+    return total_days, next_birthday
+
+
+def get_int_input(msg):
+    user_input = ""
+    done = False
+    while not done:
+        inp = input(msg)
+        if inp.isdecimal():
+            user_input = inp
+            done = True
+        else:
+            print("Please Enter digits only")
+    return int(user_input)
 
 
 if __name__ == "__main__":
-    print(Dob())
+    print(get_welcome_text())
+    birthday = get_user_birthday()
+    now = datetime.now()
+    total_days, next_birthday = calc_dates(birthday, now)
+    print(textwrap.dedent("""
+    Total days: %s
+    Next birthday is: %s    
+    """) % (total_days, next_birthday))
--- dob_cli.py
+++ dob_cli.py
@@ -1,42 +1,47 @@
-import datetime
+from datetime import datetime
 import sys
+import textwrap
+from dob import calc_dates
 
 
-class Dob:
-        	def __init__(self):
-        		self.welcome_text = """DOB : print total days since day of Birth
-        	
-        	python3 [dob_cli.py] [day] [month] [year] 
-        	
-                By: AbdulMalik Bello
-                """
-        	def calc_dob(self):
-        			if(len(sys.argv) < 4):
-        				raise Exception("ERROR: Three Command line arguments required")
-        			if(self.invalid_argument()):
-        				raise Exception("ERROR: Only Integer arguments are supported") 
-        				
-        			day, month, year = self.get_arguments()
-        			today = datetime.datetime.now()
-        			birthday = datetime.datetime(year, month, day)
-        			total_days = today - birthday
-        			
-        			return total_days
-        			
-        	def get_arguments(self):
-        		return (int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
-        	
-        	def invalid_argument(self):
-        		invalid_arg = False
-        		for i in range(1,4):
-        			if(not sys.argv[i].isdigit()): invalid_arg = True
-        		return invalid_arg
-        	
-        	def __repr__(self):
-        		print(self.welcome_text)
-        		total_days = self.calc_dob()
-        		return str(total_days)
+def get_welcome_text():
+    welcome_text = textwrap.dedent("""\
+    DOB : print total days since day of Birth
+    python3 [dob_cli.py] [day] [month] [year] 
+    By: AbdulMalik Bello\
+    """)
+    return welcome_text
+
+
+def get_user_birthday():
+    if len(sys.argv) < 4:
+        print("\nERROR: Three Command line arguments required")
+        sys.exit(1)
+    if invalid_argument():
+        print("\nERROR: Only Integer arguments are supported")
+        sys.exit(1)
+    day, month, year = get_arguments()
+    return datetime(year, month, day)
+
+
+def get_arguments():
+    return int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])
+
+
+def invalid_argument():
+    invalid_arg = False
+    for i in range(1, 4):
+        if not sys.argv[i].isdigit():
+            invalid_arg = True
+    return invalid_arg
+
 
 if __name__ == "__main__":
-    print(Dob())
-    
+    print(get_welcome_text())
+    birthday = get_user_birthday()
+    now = datetime.now()
+    total_days, next_birthday = calc_dates(birthday, now)
+    print(textwrap.dedent("""
+        Total days: %s
+        Next birthday is: %s    
+        """) % (total_days, next_birthday))
